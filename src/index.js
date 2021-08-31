539dc50b7b762de45946522696abf3ee1e183def
raduOarga <raduoarga95@gmail.com>
2021-08-31 17:56:47 +0200

    Solve first four exercises

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 // 1. Write a function that accepts an array of numbers. Return a new array with all elements doubled
 // Ex: double([1,2,3]) => [2,4,6]
 
+const double = (arr) => arr.map(num => num * 2);
+
 // 2. Write a function that accepts an array of numbers. Return a new array with only elements that are even number
 // Ex: filterEven([1,2,3,4,5,6]) => [2,4,6]
+
+const filterEven = (arr) => arr.filter(num => num % 2 === 0);
 
 // 3. Write a function that accepts an array of student object. Each student object will have a name property. Return those students that has name length larger than 5
 // Ex: const students = [{name: "abc"}, {name: "Umer Khan"}, {name: "Duy Nguyen"}]
 //     filterStudent(students) => [{name: "Umer Khan"}, {name: "Duy Nguyen"}]
 
+const filterStudent = (arr) => arr.filter(student => student.name.length > 5);
+
 // 4. Write a function that accepts an unlimited amount of arguments. The arguments are of type number. Return the largest number
 // Ex: largest(1,2,3,4,5,6) => 6
 // Hint: use Math.max()
+
+const largest = (...args) => args.reduce((a, b) => Math.max(a,b));
 
 // 5. Write a function that accepts an unlimited amount of arguments. The arguments are of type number. Return the sum of all of them
 // Ex: sum(1,2,3,4,5) => 15
