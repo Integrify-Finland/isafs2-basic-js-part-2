ca0625e7d7dd52d718134c05922ff9a67daf8431
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2021-09-01 21:00:40 +0200

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,37 +1,29 @@
 // 1. Write a function that accepts an array of numbers. Return a new array with all elements doubled
 // Ex: double([1,2,3]) => [2,4,6]
-
-const double=([1,2,3])
-double.map((item)=>item*2);
+const double = (numArray) => numArray.map((item)=>item*2);
 
 // 2. Write a function that accepts an array of numbers. Return a new array with only elements that are even number
 // Ex: filterEven([1,2,3,4,5,6]) => [2,4,6]
-
-const filterEven=([1,2,3,4,5,6]);
-filterEven.filter((item)=>item%2===0);
+const filterEven=(numArray) => numArray.filter((item)=>item % 2 === 0);
 
 // 3. Write a function that accepts an array of student object. Each student object will have a name property. Return those students that has name length larger than 5
 // Ex: const students = [{name: "abc"}, {name: "Umer Khan"}, {name: "Duy Nguyen"}]
 //     filterStudent(students) => [{name: "Umer Khan"}, {name: "Duy Nguyen"}]
 
-const students = [{name: "abc"}, {name: "Umer Khan"}, {name: "Duy Nguyen"}];
-students.filter((item)=>item.name.length>5);
+const students = (obj) => obj.filter((item)=> item.name.length > 5);
 
 // 4. Write a function that accepts an unlimited amount of arguments. The arguments are of type number. Return the largest number
 // Ex: largest(1,2,3,4,5,6) => 6
 // Hint: use Math.max()
 
-const largest =[1,2,3,4,5];
-Math.max(...largest);
+const largest = (...arg) => Math.max(...arg);
 
 // 5. Write a function that accepts an unlimited amount of arguments. The arguments are of type number. Return the sum of all of them
 // Ex: sum(1,2,3,4,5) => 15
 
-const sum = (...arr) => arr.reduce((acc,num)=>acc+num,0);
- sum(1,2,3);
+const sum = (...arr) => arr.reduce((acc,num)=> acc+num,0);
 
  // 6. You have a list of products
-
  const products = [
   { name: 'paint', category: 'household' },
   { name: 'phone', category: 'communication' },
